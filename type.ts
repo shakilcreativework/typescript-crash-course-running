87b6f0248f1f07d35d1276d39029639a29e91bb9
shakilcreativework <shakilcreativework@gmail.com>
2026-09-19 11:17:52 +0600

    Amazing, now, completed learn types

---
+++ type.ts
@@ -0,0 +1,10 @@
+const country: string = "Bangladesh";
+const n: number = 16;
+const articleStatus: boolean = true;
+
+let x;
+
+const countries: string[] = ["Bangladesh", "Iran", "Pakistan"];
+const numbers: number[] = [34, 44, 64, 353, 75, 356, 464];
+
+console.log(typeof articleStatus);
